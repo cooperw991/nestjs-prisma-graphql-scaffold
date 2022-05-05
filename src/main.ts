e74fab7edee178e48381fde9383391d4d3e8d3aa
cooperw991 <wangjx@blackbeanssoft.com>
2022-05-05 08:41:31 +0800

    build(winston, logger): config logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  Logger.log(`Api server started on: http://localhost:3004`);
 }
 bootstrap();
