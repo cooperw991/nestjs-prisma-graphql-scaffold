a0494d7d093dd0b5eeb5a9c6304b3fe771333783
cooperw991 <wangjx@blackbeanssoft.com>
2022-05-05 10:17:19 +0800

    feat(i18n): config i18n

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
+import path from 'path';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaModule } from 'nestjs-prisma';
+import { I18nModule, QueryResolver } from 'nestjs-i18n';
 
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import config from '@/configs/config';
+import { NestConfig } from '@/configs/config.interface';
 
 @Module({
   imports: [
@@ -15,6 +18,18 @@ import config from '@/configs/config';
     PrismaModule.forRoot({
       isGlobal: true,
       prismaServiceOptions: {},
+    }),
+    I18nModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        fallbackLanguage:
+          configService.get<NestConfig>('nest').fallbackLanguage,
+        loaderOptions: {
+          path: path.join(__dirname, '/i18n/'),
+          watch: true,
+        },
+      }),
+      inject: [ConfigService],
+      resolvers: [{ use: QueryResolver, options: ['lang', 'locale', 'l'] }],
     }),
   ],
   controllers: [AppController],
