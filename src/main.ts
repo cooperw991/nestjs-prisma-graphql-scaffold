3cdb8ccdf3dec76780b34d8d49fb60fb14a8b124
cooperw991 <wangjx@blackbeanssoft.com>
2022-05-05 10:29:35 +0800

    feat(main.js): config helmet and compression

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-import { WinstonModule } from 'nest-winston';
 import { Logger } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import helmet from 'helmet';
+import compression from 'compression';
 import { PrismaService } from 'nestjs-prisma';
 
+import { AppModule } from '@/app.module';
 import { NestConfig } from '@/configs/config.interface';
 import { loggerConfig } from '@/logger/logger.config';
 import { loggingMiddleware } from '@Middleware/logging.middleware';
 import { softDeleteMiddleware } from '@Middleware/soft-delete.middleware';
-import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -19,6 +21,16 @@ async function bootstrap() {
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.$use(loggingMiddleware);
   prismaService.$use(softDeleteMiddleware);
+
+  app.use(
+    helmet({
+      contentSecurityPolicy:
+        process.env.NODE_ENV === 'production' ? undefined : false,
+      crossOriginEmbedderPolicy:
+        process.env.NODE_ENV === 'production' ? true : false,
+    }),
+  );
+  app.use(compression());
 
   await app.listen(nestConfig.port, '0.0.0.0', () => {
     Logger.log(`Api server started on: http://localhost:${nestConfig.port}`);
